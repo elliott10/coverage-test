56ea0887095408690b4f81b3fadae78ea1c4b6ea
elliott10 <xiaoluoyuan@163.com>
2015-04-03 17:43:13 +0800

    fix bug

--- xaudit.sh
+++ xaudit.sh
@@ -5,6 +5,15 @@ if [ $# -ne 1 ]; then
 	exit
 fi
 
+for cmd_ in auditctl ausearch zcov date
+do
+	which $cmd_ > /dev/null 2>&1
+	if [ $? -ne 0 ]; then
+	echo -e "\"$cmd_\" command does not exist!"
+	exit
+	fi
+done
+
 echo -e "\n----- START -----\n"
 
 wpath="$1"
@@ -45,25 +54,26 @@ echo -e "finding *.gcda in $wpath"
 find_gcda=$(find $wpath -name "*.gcda")
 rm -f $find_gcda
 
-echo -e '#!/bin/bash' > ./$sec.log
-echo -e '#../configure --disable-nls CFLAGS="-g -fprofile-arcs -ftest-coverage"' >> ./$sec.log
+echo  '#!/bin/bash' > ./$sec.log
+echo  '#../configure --disable-nls CFLAGS="-g -fprofile-arcs -ftest-coverage"' >> ./$sec.log
 echo -e "\nzcov_root=$wpath" >> ./$sec.log
 echo -e "sec=$sec" >> ./$sec.log
 #echo -e "awk_cmd=\"awk 'NR==3 {gsub(/[%]/,\\\"\\\",\\\$4);print \\\$4}'\"" >> ./$sec.log
 #echo -e 'coverage_buf=([0]=init)' >> ./$sec.log
-echo -e 'declare -A coverage_buf' >> ./$sec.log
+echo  'declare -A coverage_buf' >> ./$sec.log
 
 #echo -e 'SendInfo(){
 #		echo -e "***** TimeOut *****"
 #}
 
-echo -e 'timeout_cmd(){
-		waitfor=3
+echo  'timeout_cmd(){
+		waitfor=2
 		command=$*
 		$command &
 		commandpid=$!
 
-		sleep $waitfor ; kill -9 $commandpid  > /dev/null 2>&1
+		( sleep $waitfor ; kill -9 $commandpid  > /dev/null 2>&1 )
+
 }' >> ./$sec.log
 #		( sleep $waitfor ; kill -9 $commandpid  > /dev/null 2>&1 && SendInfo ) &
 
@@ -75,24 +85,31 @@ echo -e 'timeout_cmd(){
 #}' >> ./$sec.log
 echo >> ./$sec.log
 
-echo -e 'zcov_run(){' >> ./$sec.log
+echo  'zcov_run(){' >> ./$sec.log
 
 echo -e "analyzing the $wpath/audit$sec.log"
 sudo cat $wpath/audit$sec.log | \
 awk '{if($1 == "type=EXECVE") \
-	{printf("timeout_cmd  "); \
+	{gsub(/[:.()]/,"",$3); \
+	printf("exe_%d=(",$3); \
 	for(i=6;i<=NF;i++) \
-	{printf("%s  ",substr($i,4,length($i)))} \
-	printf(" > /dev/null 2>&1"); \
-	print ""} \
+	{printf("%s ",substr($i,4,length($i)))} \
+	printf(" )\n")} \
 
 	else if($1 == "type=CWD") \
 	{print "#####"; \
-	print "cd",substr($5,5,length($5))} \
+	gsub(/[:.()]/,"",$3); \
+
+	printf("cwd_%d=",$3); \
+	printf("(cd %s \";\")\n",substr($5,5,length($5)))} \
 
 	else if($1 == "type=SYSCALL") \
 	{gsub(/[:.()]/,"",$3); \
 
+	print "all_cmd"$3"=(\"${cwd_"$3"[@]}\" \"${exe_"$3"[@]}\")"; \
+	print "${cwd_"$3"[@]}"; \
+	print "echo -e \"${exe_"$3"[@]}\""
+	print "timeout_cmd ${exe_"$3"[@]} > /dev/null 2>&1"; \
 	print ""; \
 	print "c_gcda=$(find $zcov_root -name \"*.gcda\")"; \
 	print "if [ -n \"$c_gcda\" ]; then"; \
@@ -106,28 +123,32 @@ awk '{if($1 == "type=EXECVE") \
 >> ./$sec.log
 
 echo >> ./$sec.log
-echo -e 'cd $zcov_root' >> ./$sec.log
-echo -e 'c_zcov=$(find $zcov_root -name "*.zcov")' >> ./$sec.log
-echo -e 'echo -e "zcov merge \n\033[32m$c_zcov\033[0m"' >> ./$sec.log
-echo -e 'zcov merge -f /tmp/multiple.zcov $c_zcov' >> ./$sec.log
-echo -e 'echo -e "##### summary of coverage data #####"' >> ./$sec.log
-echo -e 'zcov summarize /tmp/multiple.zcov' >> ./$sec.log
-echo -e 'echo -e "####################################"' >> ./$sec.log
+echo  'cd $zcov_root' >> ./$sec.log
+echo  'c_zcov=$(find $zcov_root -name "*.zcov")' >> ./$sec.log
+echo  'echo -e "zcov merge \n$c_zcov"' >> ./$sec.log
+echo  'zcov merge -f /tmp/multiple.zcov $c_zcov' >> ./$sec.log
+echo  'echo -e "##### summary of coverage data #####"' >> ./$sec.log
+echo  'zcov summarize /tmp/multiple.zcov' >> ./$sec.log
+echo  'echo -e "####################################"' >> ./$sec.log
 
-echo -e "mkdir -p /tmp/$sec/all_zcov; mv \$c_zcov /tmp/$sec/all_zcov/" >> ./$sec.log
+echo  'mkdir -p /tmp/$sec/all_zcov; mv $c_zcov /tmp/$sec/all_zcov/' >> ./$sec.log
 
-echo -e "echo -e \"\${!coverage_buf[@]}\" > /tmp/$sec/coverage_buf_ind" >> ./$sec.log
-echo -e "echo -e \"\${coverage_buf[@]}\" > /tmp/$sec/coverage_buf_val" >> ./$sec.log
-
-echo -e '} #zcov_run()' >> ./$sec.log
+echo  'echo -e "${!coverage_buf[@]}" > /tmp/$sec/coverage_buf_index' >> ./$sec.log
+echo  'echo -e "${coverage_buf[@]}" > /tmp/$sec/coverage_buf_value' >> ./$sec.log
+echo  '} #zcov_run()' >> ./$sec.log
 
 #echo -e 'f_name=$(echo $0 |sed "s/\.log//g")
-echo -e 'if [ -d /tmp/$sec ]
+echo  'if [ -d /tmp/$sec ]
 then
 	echo -e "No run zcov_run()"
-	for j in $(cat /tmp/$sec/coverage_buf_ind)
+
+	coverage_value=(`cat /tmp/$sec/coverage_buf_value`)
+	value_tmp=0
+
+	for j in $(cat /tmp/$sec/coverage_buf_index)
 	do
-		coverage_buf[$j]=100
+		coverage_buf[$j]=${coverage_value[$value_tmp]}
+		let "value_tmp+=1"
 	done
 
 else
@@ -136,31 +157,36 @@ else
 
 fi' >> ./$sec.log
 
-echo -e "cd /tmp/$sec/all_zcov" >> ./$sec.log
+echo  'cd /tmp/$sec/all_zcov' >> ./$sec.log
 
-echo -e 'for i in "${!coverage_buf[@]}"
+echo  'for i in ${!coverage_buf[@]}
 do
 	if [ $(echo "${coverage_buf[$i]} < 5" | bc) -eq 1 ]
 	then 
 		unset coverage_buf[$i]
 	fi   
 done' >> ./$sec.log
-echo -e 'echo -e "Coverage :"' >> ./$sec.log
-echo -e 'for c in "${!coverage_buf[@]}"
+echo  'echo -e "Coverage :"' >> ./$sec.log
+
+echo  'for c in ${!coverage_buf[@]}
 do
-	echo -n "$c.zcov-->${coverage_buf[$c]} "
+	eval echo -e "$c.zcov ${coverage_buf[$c]} \\\033[32m\${all_cmd$c[@]}\\\033[0m" >> /tmp/$sec/tmp_info
 done' >> ./$sec.log
-echo -e 'echo' >> ./$sec.log
-echo -e 'echo -e "More test data in /tmp/$sec"' >> ./$sec.log
+echo 'sort -n -k 2,2 /tmp/$sec/tmp_info; rm /tmp/$sec/tmp_info' >> ./$sec.log
+echo  'echo -e "####################################"' >> ./$sec.log
+echo  'echo' >> ./$sec.log
+echo  'echo -e "More test data in /tmp/$sec"' >> ./$sec.log
 
-echo -e 'echo -e "Will continue to test coverage ..."' >> ./$sec.log
-echo -e 'read user_input' >> ./$sec.log
+echo  'echo -e "Will continue to test coverage ..."' >> ./$sec.log
+#pause break
+#echo  'read user_input' >> ./$sec.log
 
-echo -e 'echo -e "Calculating 3 largest combination coverage ..."' >> ./$sec.log
-echo -e "max_buf=(0 0 0 0 0 0 0 0 0 0)" >> ./$sec.log
-echo -e 'for a in ${!coverage_buf[@]}
+echo  'echo -e "Calculating 3 largest combination coverage ..."' >> ./$sec.log
+echo  'max_buf=(0 0 0 0 0)' >> ./$sec.log
+echo  'for a in ${!coverage_buf[@]}
 do
-	echo -n "$a.zcov "
+	#echo -n "$a  "
+	echo -n "."
 	for b in ${!coverage_buf[@]}
 	do
 		if [ "$b" -eq "$a" ]
@@ -179,22 +205,12 @@ echo -e "		#echo -e \"1:\$a.zcov 2:\$b.zcov 3:\$c.zcov\"
 			zcov merge -f t.zcov  \$a.zcov \$b.zcov \$c.zcov
 			t_zcov=\$(zcov summarize t.zcov |awk 'NR==3 {gsub(/[%]/,\"\",\$4);print \$4}')" >> ./$sec.log
 
-echo -e '		if [ $(echo "$t_zcov > ${max_buf[2]}" | bc) -eq 1 ]
+echo  '		if [ $(echo "$t_zcov > ${max_buf[0]}" | bc) -eq 1 ]
 			then
-				max_buf[2]=$t_zcov
-				max_buf[1]=$a.$b.$c
-
-			else if [ $(echo "$t_zcov > ${max_buf[4]}" | bc) -eq 1 ]
-				then
-					max_buf[4]=$t_zcov
-					max_buf[3]=$a.$b.$c
-
-				else if [ $(echo "$t_zcov > ${max_buf[6]}" | bc) -eq 1 ]
-					then
-						max_buf[6]=$t_zcov
-						max_buf[5]=$a.$b.$c
-					fi
-				fi
+				max_buf[1]=$a
+				max_buf[2]=$b
+				max_buf[3]=$c
+				max_buf[0]=$t_zcov
 			fi
 		done
 	done
@@ -202,10 +218,13 @@ done
 echo 
 echo -e "------------------------------"
 echo -e "[ ${max_buf[@]} ]"
+eval echo -e "${max_buf[1]}.zcov \\\033[32m\${all_cmd${max_buf[1]}[@]}\\\033[0m"
+eval echo -e "${max_buf[2]}.zcov \\\033[32m\${all_cmd${max_buf[2]}[@]}\\\033[0m"
+eval echo -e "${max_buf[3]}.zcov \\\033[32m\${all_cmd${max_buf[3]}[@]}\\\033[0m"
 echo -e "------------------------------"
 date +"%F %T"' >> ./$sec.log
 
-echo -e 'echo -e "$0 exit."' >> ./$sec.log
+echo  'echo -e "$0 exit."' >> ./$sec.log
 
 if [ -n "$find_gcda" ]; then
 	sudo chmod 755 ./$sec.log
